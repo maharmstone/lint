53a453ef76a2f912254de53ca80d7a502ddfb595
Mark Harmstone <mark@harmstone.com>
2020-07-11 22:50:52 +0100

    init: create Machine and System volatile keys

--- init/init.c
+++ init/init.c
@@ -1,13 +1,15 @@
 #include <muw.h>
 #include <stdio.h>
 
-int main() {
+#define STATUS_SUCCESS 0x00000000
+
+static NTSTATUS mount_system_hive() {
     NTSTATUS Status;
     UNICODE_STRING file_us, key_us;
     OBJECT_ATTRIBUTES key, file;
 
     static const WCHAR file_str[] = L"\\Device\\UnixRoot\\root\\temp\\init\\SYSTEM"; // FIXME
-    static const WCHAR key_str[] = L"\\Registry\\Machine";
+    static const WCHAR key_str[] = L"\\Registry\\Machine\\System";
 
     file_us.Length = file_us.MaximumLength = sizeof(file_str) - sizeof(WCHAR);
     file_us.Buffer = (WCHAR*)file_str;
@@ -32,8 +34,64 @@ int main() {
     Status = NtLoadKey(&key, &file);
     if (!NT_SUCCESS(Status)) {
         fprintf(stderr, "NtLoadKey returned %08x.\n", (int32_t)Status);
-        return 1;
+        return Status;
     }
+
+    return Status;
+}
+
+static NTSTATUS create_reg_keys() {
+    NTSTATUS Status;
+    HANDLE h;
+    OBJECT_ATTRIBUTES oa;
+    UNICODE_STRING us;
+    ULONG dispos;
+
+    static const WCHAR machine[] = L"\\Registry\\Machine";
+    static const WCHAR system[] = L"\\Registry\\Machine\\System";
+
+    us.Length = us.MaximumLength = sizeof(machine) - sizeof(WCHAR);
+    us.Buffer = (WCHAR*)machine;
+
+    oa.Length = sizeof(oa);
+    oa.RootDirectory = NULL;
+    oa.ObjectName = &us;
+    oa.Attributes = 0;
+    oa.SecurityDescriptor = NULL;
+    oa.SecurityQualityOfService = NULL;
+
+    Status = NtCreateKey(&h, 0, &oa, 0, NULL, REG_OPTION_VOLATILE, &dispos);
+    if (!NT_SUCCESS(Status)) {
+        printf("NtCreateKey returned %08x\n", (int32_t)Status);
+        return Status;
+    }
+
+    NtClose(h);
+
+    us.Length = us.MaximumLength = sizeof(system) - sizeof(WCHAR);
+    us.Buffer = (WCHAR*)system;
+
+    Status = NtCreateKey(&h, 0, &oa, 0, NULL, REG_OPTION_VOLATILE, &dispos);
+    if (!NT_SUCCESS(Status)) {
+        printf("NtCreateKey returned %08x\n", (int32_t)Status);
+        return Status;
+    }
+
+    NtClose(h);
+
+    return STATUS_SUCCESS;
+}
+
+int main() {
+    NTSTATUS Status;
+
+    Status = create_reg_keys();
+    if (!NT_SUCCESS(Status))
+        return 1;
+
+    Status = mount_system_hive();
+    if (!NT_SUCCESS(Status))
+        return 1;
 
     return 0;
 }
