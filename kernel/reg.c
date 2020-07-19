77e4931a788f0557c10e2e3ba070724a76e477e8
Mark Harmstone <mark@harmstone.com>
2020-07-19 23:51:16 +0100

    NtDeleteValueKey: update MaxValueDataLen in parent

--- kernel/reg.c
+++ kernel/reg.c
@@ -2531,6 +2531,7 @@ static NTSTATUS NtDeleteValueKey(HANDLE KeyHandle, PUNICODE_STRING ValueName) {
                 free_cell(key->h, kn->Values, key->is_volatile);
                 kn->Values = 0;
                 kn->MaxValueNameLen = 0;
+                kn->MaxValueDataLen = 0;
             } else {
                 int32_t old_size = -*(int32_t*)((uint8_t*)bins + kn->Values);
                 int32_t new_size = ((kn->ValuesCount - 1) * sizeof(uint32_t)) + sizeof(int32_t);
@@ -2551,6 +2552,7 @@ static NTSTATUS NtDeleteValueKey(HANDLE KeyHandle, PUNICODE_STRING ValueName) {
                     bins = key->is_volatile ? key->h->volatile_bins : key->h->bins;
                     kn = (CM_KEY_NODE*)((uint8_t*)bins + key->offset + sizeof(int32_t));
                     values_list = (uint32_t*)((uint8_t*)bins + kn->Values + sizeof(int32_t));
+                    vk = (CM_KEY_VALUE*)((uint8_t*)bins + values_list[i] + sizeof(int32_t));
 
                     new_values_list = (uint32_t*)((uint8_t*)bins + values_list_offset + sizeof(int32_t));
 
@@ -2560,7 +2562,7 @@ static NTSTATUS NtDeleteValueKey(HANDLE KeyHandle, PUNICODE_STRING ValueName) {
                     kn->Values = values_list_offset;
                 }
 
-                if (ValueName->Length == kn->MaxValueNameLen) {
+                if (kn->MaxValueNameLen  == ValueName->Length) {
                     unsigned int j;
 
                     kn->MaxValueNameLen = 0;
@@ -2583,6 +2585,29 @@ static NTSTATUS NtDeleteValueKey(HANDLE KeyHandle, PUNICODE_STRING ValueName) {
 
                                 if (len > kn->MaxValueNameLen)
                                     kn->MaxValueNameLen = len;
+                            }
+                        }
+                    }
+                }
+
+                if (kn->MaxValueDataLen == (vk->DataLength & ~CM_KEY_VALUE_SPECIAL_SIZE)) {
+                    unsigned int j;
+
+                    kn->MaxValueDataLen = 0;
+
+                    for (j = 0; j < kn->ValuesCount; j++) {
+                        if (j != i) {
+                            CM_KEY_VALUE* vk2 = (CM_KEY_VALUE*)((uint8_t*)bins + values_list[j] + sizeof(int32_t));
+
+                            // FIXME - check not out of bounds
+
+                            size = -*(int32_t*)((uint8_t*)bins + values_list[i]);
+
+                            if (vk2->Signature == CM_KEY_VALUE_SIGNATURE || size >= sizeof(int32_t) + offsetof(CM_KEY_VALUE, Name[0]) + vk2->NameLength) {
+                                uint32_t len = vk2->DataLength & ~CM_KEY_VALUE_SPECIAL_SIZE;
+
+                                if (len > kn->MaxValueDataLen)
+                                    kn->MaxValueDataLen = len;
                             }
                         }
                     }
