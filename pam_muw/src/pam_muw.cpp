e33bba99cacca796450fe711a40b427bbc84e9cd
Mark Harmstone <mark@harmstone.com>
2020-07-22 03:05:10 +0100

    pam_muw: unmounted hives even if we think we didn't mount them

--- pam_muw/src/pam_muw.cpp
+++ pam_muw/src/pam_muw.cpp
@@ -21,8 +21,6 @@ using namespace std;
 typedef struct {
     uid_t uid;
     unsigned int num_sessions;
-    bool hive_mounted;
-    bool classes_mounted;
 } entry;
 
 typedef struct {
@@ -108,33 +106,27 @@ static void release_spinlock(shared* sh) {
     sh->turn++;
 }
 
-static void first_session(pam_handle_t* pamh, entry* ent, struct passwd* pwd) {
+static void first_session(pam_handle_t* pamh, struct passwd* pwd) {
     if (pwd->pw_uid != 1000)
         return; // FIXME
 
     u16string sidstr = u"S-1-5-21-0-0-0-1000"; // FIXME - derive from uid
 
     try {
-        if (!ent->hive_mounted) {
-            create_reg_key(u"\\Registry\\User\\" + sidstr, true);
+        create_reg_key(u"\\Registry\\User\\" + sidstr, true);
 
-            // FIXME - make paths to hives customizable?
-            // FIXME - if doesn't exist, create new hive (copy from DEFAULT and recursively change SIDs)
-            mount_hive(u"\\Registry\\User\\" + sidstr, get_nt_path(string(pwd->pw_dir) + "/.wine/NTUSER.DAT"));
-            ent->hive_mounted = true;
-        }
+        // FIXME - make paths to hives customizable?
+        // FIXME - if doesn't exist, create new hive (copy from DEFAULT and recursively change SIDs)
+        mount_hive(u"\\Registry\\User\\" + sidstr, get_nt_path(string(pwd->pw_dir) + "/.wine/NTUSER.DAT"));
 
-        if (!ent->classes_mounted) {
-            create_reg_key(u"\\Registry\\User\\" + sidstr + u"_Classes", true);
+        create_reg_key(u"\\Registry\\User\\" + sidstr + u"_Classes", true);
 
-            // FIXME - if doesn't exist, initialize blank hive(?)
-            mount_hive(u"\\Registry\\User\\" + sidstr + u"_Classes", get_nt_path(string(pwd->pw_dir) + "/.wine/UsrClass.dat"));
-            ent->classes_mounted = true;
+        // FIXME - if doesn't exist, initialize blank hive(?)
+        mount_hive(u"\\Registry\\User\\" + sidstr + u"_Classes", get_nt_path(string(pwd->pw_dir) + "/.wine/UsrClass.dat"));
 
-            create_reg_key(u"\\Registry\\User\\" + sidstr + u"\\Software", false); // should already exist
-            create_reg_symlink(u"\\Registry\\User\\" + sidstr + u"\\Software\\Classes",
-                               u"\\Registry\\User\\" + sidstr + u"_Classes");
-        }
+        create_reg_key(u"\\Registry\\User\\" + sidstr + u"\\Software", false); // should already exist
+        create_reg_symlink(u"\\Registry\\User\\" + sidstr + u"\\Software\\Classes",
+                           u"\\Registry\\User\\" + sidstr + u"_Classes");
     } catch (const exception& e) {
         pam_syslog(pamh, LOG_ERR, "first_session: %s\n", e.what());
     }
@@ -182,10 +174,8 @@ int pam_sm_open_session(pam_handle_t* pamh, __attribute__((unused)) int flags,
 
         ent->uid = pwd->pw_uid;
         ent->num_sessions = 1;
-        ent->hive_mounted = false;
-        ent->classes_mounted = false;
 
-        first_session(pamh, ent, pwd);
+        first_session(pamh, pwd);
 
         sh->num_entries++;
     }
@@ -199,30 +189,26 @@ int pam_sm_open_session(pam_handle_t* pamh, __attribute__((unused)) int flags,
     return PAM_IGNORE;
 }
 
-static void last_session(pam_handle_t* pamh, entry* ent, struct passwd* pwd) {
+static void last_session(pam_handle_t* pamh, struct passwd* pwd) {
     if (pwd->pw_uid != 1000)
         return; // FIXME
 
     u16string sidstr = u"S-1-5-21-0-0-0-1000"; // FIXME - derive from uid
 
-    if (ent->classes_mounted) {
-        try {
-            unmount_hive(u"\\Registry\\User\\" + sidstr + u"_Classes");
+    try {
+        unmount_hive(u"\\Registry\\User\\" + sidstr + u"_Classes");
 
-            delete_key(u"\\Registry\\User\\" + sidstr + u"_Classes");
-        } catch (const exception& e) {
-            pam_syslog(pamh, LOG_ERR, "last_session: %s\n", e.what());
-        }
+        delete_key(u"\\Registry\\User\\" + sidstr + u"_Classes");
+    } catch (const exception& e) {
+        pam_syslog(pamh, LOG_ERR, "last_session: %s\n", e.what());
     }
 
-    if (ent->hive_mounted) {
-        try {
-            unmount_hive(u"\\Registry\\User\\" + sidstr);
+    try {
+        unmount_hive(u"\\Registry\\User\\" + sidstr);
 
-            delete_key(u"\\Registry\\User\\" + sidstr);
-        } catch (const exception& e) {
-            pam_syslog(pamh, LOG_ERR, "last_session: %s\n", e.what());
-        }
+        delete_key(u"\\Registry\\User\\" + sidstr);
+    } catch (const exception& e) {
+        pam_syslog(pamh, LOG_ERR, "last_session: %s\n", e.what());
     }
 
     close_muwine();
@@ -268,7 +254,7 @@ int pam_sm_close_session(__attribute__((unused)) pam_handle_t* pamh, __attribute
                 memcpy(&sh->entries[i], &sh->entries[i+1], sizeof(entry) * (sh->num_entries - i - 1));
                 sh->num_entries--;
 
-                last_session(pamh, &ent, pwd);
+                last_session(pamh, pwd);
             }
 
             break;
