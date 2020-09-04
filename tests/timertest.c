51938d02f4ddd77df5150a6357516aaa63321b5a
Mark Harmstone <mark@harmstone.com>
2020-09-04 22:14:16 +0100

    timertest: also test NtOpenTimer

--- tests/timertest.c
+++ tests/timertest.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <uchar.h>
 
 #ifdef _WIN32
 
@@ -32,30 +33,53 @@ NTSTATUS __stdcall NtSetTimer(HANDLE TimerHandle, PLARGE_INTEGER DueTime,
 
 int main() {
     NTSTATUS Status;
-    HANDLE h;
+    HANDLE h, h2;
     LARGE_INTEGER time;
+    UNICODE_STRING us;
+    OBJECT_ATTRIBUTES oa;
     unsigned int i = 0;
 
-    Status = NtCreateTimer(&h, MAXIMUM_ALLOWED, NULL, SynchronizationTimer);
+    static const char16_t path[] = u"\\clock";
+
+    us.Length = us.MaximumLength = sizeof(path) - sizeof(char16_t);
+    us.Buffer = (WCHAR*)path;
+
+    oa.Length = sizeof(oa);
+    oa.RootDirectory = NULL;
+    oa.ObjectName = &us;
+    oa.Attributes = 0;
+    oa.SecurityDescriptor = NULL;
+    oa.SecurityQualityOfService = NULL;
+
+    Status = NtCreateTimer(&h, MAXIMUM_ALLOWED, &oa, SynchronizationTimer);
     if (!NT_SUCCESS(Status)) {
         fprintf(stderr, "NtCreateTimer returned %08x\n", (int32_t)Status);
         return 1;
     }
 
+    Status = NtOpenTimer(&h2, MAXIMUM_ALLOWED, &oa);
+    if (!NT_SUCCESS(Status)) {
+        fprintf(stderr, "NtOpenTimer returned %08x\n", (int32_t)Status);
+        NtClose(h);
+        return 1;
+    }
+
     time.QuadPart = -10000000;
 
-    Status = NtSetTimer(h, &time, NULL, NULL, false, 1000, NULL);
+    Status = NtSetTimer(h2, &time, NULL, NULL, false, 1000, NULL);
     if (!NT_SUCCESS(Status)) {
         fprintf(stderr, "NtSetTimer returned %08x\n", (int32_t)Status);
+        NtClose(h2);
         NtClose(h);
         return 1;
     }
 
     i = 0;
     do {
-        Status = NtWaitForSingleObject(h, false, NULL);
+        Status = NtWaitForSingleObject(h2, false, NULL);
         if (Status != STATUS_WAIT_0) {
             fprintf(stderr, "NtWaitForSingleObject returned %08x\n", (int32_t)Status);
+            NtClose(h2);
             NtClose(h);
             return 1;
         }
@@ -65,15 +89,17 @@ int main() {
         i++;
 
         if (i == 3) {
-            Status = NtCancelTimer(h, NULL);
+            Status = NtCancelTimer(h2, NULL);
             if (!NT_SUCCESS(Status)) {
                 fprintf(stderr, "NtCancelTimer returned %08x\n", (int32_t)Status);
+                NtClose(h2);
                 NtClose(h);
                 return 1;
             }
         }
     } while (true);
 
+    NtClose(h2);
     NtClose(h);
 
     return 0;
