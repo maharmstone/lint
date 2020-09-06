3bbfa9f8e438d650329e9215c39bbe06867a1e9d
Mark Harmstone <mark@harmstone.com>
2020-09-06 20:22:21 +0100

    NtOpenSemaphore: implement

--- kernel/semaphore.c
+++ kernel/semaphore.c
@@ -150,12 +150,82 @@ NTSTATUS user_NtCreateSemaphore(PHANDLE SemaphoreHandle, ACCESS_MASK DesiredAcce
 
 static NTSTATUS NtOpenSemaphore(PHANDLE SemaphoreHandle, ACCESS_MASK DesiredAccess,
                                 POBJECT_ATTRIBUTES ObjectAttributes) {
-    printk(KERN_INFO "NtOpenSemaphore(%px, %x, %px): stub\n",
-           SemaphoreHandle, DesiredAccess, ObjectAttributes);
+    NTSTATUS Status;
+    UNICODE_STRING us, after;
+    WCHAR* oa_us_alloc = NULL;
+    sem_object* sem;
+    ACCESS_MASK access;
+    bool after_alloc = false;
 
-    // FIXME
+    if (!ObjectAttributes || ObjectAttributes->Length < sizeof(OBJECT_ATTRIBUTES) || !ObjectAttributes->ObjectName)
+        return STATUS_INVALID_PARAMETER;
 
-    return STATUS_NOT_IMPLEMENTED;
+    if (ObjectAttributes->RootDirectory) {
+        ACCESS_MASK access;
+        object_header* obj = get_object_from_handle(ObjectAttributes->RootDirectory, &access);
+
+        if (!obj)
+            return STATUS_INVALID_HANDLE;
+
+        if (obj->type != dir_type) {
+            dec_obj_refcount(obj);
+            return STATUS_INVALID_HANDLE;
+        }
+
+        spin_lock(&obj->path_lock);
+
+        us.Length = obj->path.Length + sizeof(WCHAR) + ObjectAttributes->ObjectName->Length;
+        us.Buffer = oa_us_alloc = kmalloc(us.Length, GFP_KERNEL);
+
+        if (!us.Buffer) {
+            spin_unlock(&obj->path_lock);
+            dec_obj_refcount(obj);
+            return STATUS_INSUFFICIENT_RESOURCES;
+        }
+
+        memcpy(us.Buffer, obj->path.Buffer, obj->path.Length);
+        us.Buffer[obj->path.Length / sizeof(WCHAR)] = '\\';
+        memcpy(&us.Buffer[(obj->path.Length / sizeof(WCHAR)) + 1], ObjectAttributes->ObjectName->Buffer,
+               ObjectAttributes->ObjectName->Length);
+
+        spin_unlock(&obj->path_lock);
+
+        dec_obj_refcount(obj);
+    } else {
+        us.Length = ObjectAttributes->ObjectName->Length;
+        us.Buffer = ObjectAttributes->ObjectName->Buffer;
+    }
+
+    Status = muwine_open_object(&us, (object_header**)&sem, &after, &after_alloc, false);
+    if (!NT_SUCCESS(Status))
+        goto end;
+
+    if (sem->header.h.type != sem_type || after.Length != 0) {
+        dec_obj_refcount(&sem->header.h);
+        Status = STATUS_INVALID_PARAMETER;
+        goto end;
+    }
+
+    access = sanitize_access_mask(DesiredAccess, sem_type);
+
+    // FIXME - check against SD
+
+    if (access == MAXIMUM_ALLOWED)
+        access = SEMAPHORE_ALL_ACCESS; // FIXME - should only be what SD allows
+
+    Status = muwine_add_handle(&sem->header.h, SemaphoreHandle, ObjectAttributes->Attributes & OBJ_KERNEL_HANDLE, access);
+
+    if (!NT_SUCCESS(Status))
+        dec_obj_refcount(&sem->header.h);
+
+end:
+    if (oa_us_alloc)
+        kfree(oa_us_alloc);
+
+    if (after_alloc)
+        kfree(after.Buffer);
+
+    return Status;
 }
 
 NTSTATUS user_NtOpenSemaphore(PHANDLE SemaphoreHandle, ACCESS_MASK DesiredAccess,
